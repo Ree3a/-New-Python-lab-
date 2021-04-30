dd7ac3411734c93a6892fe3cf40b5a4e10202ee9
Dell <riyastha406@gmail.com>
2021-04-30 10:13:30 +0545

    ques3

---
+++ ques3.py
@@ -0,0 +1,8 @@
+'''
+Write a program that reads the length of the base and the height of a right-angled triangle and prints the area .
+Every number is given in a separate line.
+'''
+base_value = int(input("Enter the base of the triangle:"))
+height_value = int(input("Enter the height of the trianle:"))
+area = (base_value * height_value)//2
+print(f"The area of right angles triangle is {area}")
